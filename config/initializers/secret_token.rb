a857f2a044e906ee556f0f71480cef3f88c84089
Michael Whittaker <post@michael-whittaker.de>
2013-04-13 21:03:53 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MockDiscuss::Application.config.secret_token = '9df46d70ad2f3f569ff813f4ad4a37be9072fbc31d77ed2b8b9bea14d0192c4ef0c0348a51783914854453b41d25aea96da8b3e53967ba746f41a256e068d7fc'
